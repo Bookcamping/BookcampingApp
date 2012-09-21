b7212d03cc6fac42c44fdb03b222d8ce48a34c84
danigb <danigb@gmail.com>
2012-09-21 10:09:42 +0200

    Mejorando los comentarios

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,17 @@
+# encoding: utf-8
 class CommentsController < ApplicationController
-  expose(:comments)
+  expose(:comments) { Comment.all }
   expose(:comment)
 
+  def index
+  end
+
   def show
+    if comment.resource_type == 'Post'
+      redirect_to root_path, alert: 'Era un comentario al blog. Visítalo en blog.bookcamping.cc'
+    else
     redirect_to comment.resource
+    end
   end
 
 end
