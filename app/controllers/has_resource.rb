bbd9720f1d60b486b57de84ba6e73e6c33d7fb78
danigb <danigb@gmail.com>
2012-10-03 17:34:54 +0200

    i18n bug fix

--- app/controllers/has_resource.rb
+++ app/controllers/has_resource.rb
@@ -58,7 +58,7 @@ module HasResource
   end
 
   def flash_for(action, name)
-    flash[:notice] = t("#{name}s.#{action}d", default: "resource.#{action}d")
+    flash[:notice] = t("#{name}s.#{action}d", default: t("resource.#{action}d"))
   end
 
   private
